da9f1a3a8db8620eb5f2e82bbad7ed487ab50e6a
Mytresh-gowd <mytreshgowd@gmail.com>
2026-03-08 12:29:53 +0530

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBw5fT6BAL3sXrn4VJDuRijqXLTR0KetEA",
-  authDomain: "spreadsheet-demo-af666.firebaseapp.com",
-  projectId: "spreadsheet-demo-af666",
-  storageBucket: "spreadsheet-demo-af666.firebasestorage.app",
-  messagingSenderId: "925741334456",
-  appId: "1:925741334456:web:f0180c7cc9db8d1fe1994d"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 }
 
 const app = initializeApp(firebaseConfig)
